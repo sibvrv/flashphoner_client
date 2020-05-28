35c912a65a144d7d9d7bd92e9d762c394a9e266e
Roman Vereshchagin <info@sibvrv.com>
2020-05-28 11:52:30 +0700

    WebRTC-Media-Provider: change width / height attributes

--- src/webrtc-media-provider.js
+++ src/webrtc-media-provider.js
@@ -1169,11 +1169,11 @@ function normalizeConstraints(constraints) {
             var height = constraints.video.height;
             if (browserDetails.browser == "safari") {
                 if (!width || !height) {
-                    constraints.video.width = {min: 320, max: 640};
-                    constraints.video.height = {min: 240, max: 480};
+                    constraints.video.width = {ideal: 320};
+                    constraints.video.height = {ideal: 240};
                 } else if (typeof width !== 'object' || typeof height !== 'object') {
-                    constraints.video.width = {min: width, max: width};
-                    constraints.video.height = {min: height, max: height};
+                    constraints.video.width = {ideal: width};
+                    constraints.video.height = {ideal: height};
                 }
             } else if (isNaN(width) || width === 0 || isNaN(height) || height === 0) {
                 constraints.video.width = 320;
